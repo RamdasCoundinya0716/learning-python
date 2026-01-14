98fef7b675af557b5e6fd66e66dec99d545ca8a2
Ramdas Coundinya <rcoundinya@gmail.com>
2026-01-14 09:05:39 +0530

    added additonal requirements

--- modularizing-python-code - ch.2/mycalcfunctions.py
+++ modularizing-python-code - ch.2/mycalcfunctions.py
@@ -1,21 +1,26 @@
 from random_num_gen import generate_rand_int
 from calc import add, subtract, multiply, divide, power, modulus
 
+def operations():
+    sum = add(x, y)
+    difference = subtract(x, y)
+    product = multiply(x, y)
+    quotient = round(divide(x, y),2)
+    exp = power(x, y)
+    mod = modulus(x, y)
+
+    print(f"Chosen random numbers: x={x}, y={y}")
+
+    print(f"Addition: {x} + {y} = {sum}")
+    print(f"Subtraction: {x} - {y} = {difference}")
+    print(f"Multiplication: {x} * {y} = {product}")
+    print(f"Division: {x} / {y} = {quotient}")
+    print(f"Exponentiation: {x} ** {y} = {exp}")
+    print(f"Modulus: {x} % {y} = {mod}")
+
 x = generate_rand_int(1, 10)
 y = generate_rand_int(1, 10)
 
-sum = add(x, y)
-difference = subtract(x, y)
-product = multiply(x, y)
-quotient = divide(x, y)
-exp = power(x, y)
-mod = modulus(x, y)
 
-print(f"Chosen random numbers: x={x}, y={y}")
-
-print(f"Addition: {x} + {y} = {sum}")
-print(f"Subtraction: {x} - {y} = {difference}")
-print(f"Multiplication: {x} * {y} = {product}")
-print(f"Division: {x} / {y} = {quotient}")
-print(f"Exponentiation: {x} ** {y} = {exp}")
-print(f"Modulus: {x} % {y} = {mod}")
+if __name__ == "__main__":
+    operations()
